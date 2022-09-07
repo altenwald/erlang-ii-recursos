833f6a850780a42f794d931603a4e137b932ae19
Manuel Rubio <manuel@altenwald.com>
2022-09-07 14:32:35 +0200

    segunda edición, cambios menores en capítulo 3

--- cap3/tcpsrv/echo.erl
+++ cap3/tcpsrv/echo.erl
@@ -6,6 +6,6 @@ start(Port) ->
 
 handle_request(Socket, Msg) ->
     io:format("Recibido ~p: ~p~n", [self(), Msg]),
-    timer:sleep(5000), %% 5 segundos de espera
+    timer:sleep(5_000), %% 5 segundos de espera
     tcpsrv:send(Socket, io_lib:format("Eco: ~s", [Msg])),
     ok.
