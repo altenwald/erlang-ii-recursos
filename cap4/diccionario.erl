2bdccd9f6a16938796b55977badc538d200ec909
Manuel Rubio <manuel@altenwald.com>
2022-10-02 03:48:27 +0200

    corrige ejemplos de capítulo 4

--- cap4/diccionario.erl
+++ cap4/diccionario.erl
@@ -14,10 +14,7 @@
     init/1,
     handle_call/3,
     handle_cast/2,
-    handle_info/2,
-    terminate/2,
-    code_change/3,
-    format_status/2
+    handle_info/2
 ]).
 
 -define(TIMEOUT, 5000).
@@ -26,33 +23,22 @@ start_link() ->
     gen_server:start_link(?MODULE, [], []).
 
 stop(Server) ->
-    gen_server:cast(Server, stop).
+    gen_server:stop(Server).
 
 get_value(Server, Key) ->
-    gen_server:call(Server, {get, Key}).
+    gen_server:call(Server, {get_value, Key}).
 
 add_value(Server, Key, Value) ->
-    gen_server:cast(Server, {add, Key, Value}).
+    gen_server:cast(Server, {add_value, Key, Value}).
 
 init([]) ->
-    {ok, dict:new(), ?TIMEOUT}.
+    {ok, #{}, ?TIMEOUT}.
 
-handle_call({get, Key}, _From, Dict) ->
-    {reply, dict:find(Key, Dict), Dict, ?TIMEOUT}.
+handle_call({get_value, Key}, _From, Map) ->
+    {reply, maps:get(Key, Map, undefined), Map, ?TIMEOUT}.
 
-handle_cast({add, Key, Value}, Dict) ->
-    {noreply, dict:store(Key, Value, Dict)};
-handle_cast(stop, Dict) ->
-    {stop, normal, Dict}.
+handle_cast({add_value, Key, Value}, Map) ->
+    {noreply, maps:put(Key, Value, Map), ?TIMEOUT}.
 
 handle_info(timeout, State) ->
     {noreply, State, hibernate}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-format_status(_Type, [_Dict, Dict]) ->
-    dict:to_list(Dict).
